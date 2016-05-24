e5260810ebcc91d01c5300125a70ab831e2a891d
dilara91 <dilara91@users.noreply.github.com>
2016-05-24 18:35:04 +0300

    Update hmm_train.py

--- hmm_train.py
+++ hmm_train.py
@@ -78,14 +78,14 @@ def main(filename = fname,tagStyle = tStyle):
 		
 	
 def create_tag_file(tagCount,tagStyle):
-	result_file = codecs.open('tags'+tagStyle+'.txt','a+',encoding='utf8')
+	result_file = codecs.open('tags'+tagStyle+'.txt','w+',encoding='utf8')
 	for tag in tagCount.keys():
 		result_file.write(tag)
 		result_file.write('\n')
 	result_file.close()
 	
 def create_dict(words):
-	dictionary = codecs.open('dictionary.txt','a+',encoding='utf8')
+	dictionary = codecs.open('dictionary.txt','w+',encoding='utf8')
 	for word in words:
 		dictionary.write(word)
 		dictionary.write('\n')
